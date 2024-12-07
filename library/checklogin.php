4406356820098965afab3b707adbecb5bb2cd5a1
ahmadsofuwan <sofuwan62@gmail.com>
2024-12-07 15:01:27 +0700

    test update

--- library/checklogin.php
+++ library/checklogin.php
@@ -27,11 +27,12 @@ function session_verify() {
 	session_start();
 
 	$REMOTE_ADDR = $_SERVER['REMOTE_ADDR'];
-	if (substr(md5($REMOTE_ADDR), 0, 10+substr(session_id(), 0, 1)) == 
-		substr(session_id(), 1, 10+substr(session_id(), 0, 1))) {
-		$session_valid="yes";
+	$session_id_first_char = (int)substr(session_id(), 0, 1);
+	if (substr(md5($REMOTE_ADDR), 0, 10 + $session_id_first_char) == 
+		substr(session_id(), 1, 10 + $session_id_first_char)) {
+		$session_valid = "yes";
 	} else {
-		$session_valid="no";
+		$session_valid = "no";
 	}
 
 	return $session_valid;
